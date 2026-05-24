a53b509401d34ad647dfcc07f6f58bc87ac97645
DESKTOP-IJPDF89\USUARIO <correo@correo.com>
2026-05-23 22:34:56 -0500

    Modificar build

--- k8s-deployment/build.sh
+++ k8s-deployment/build.sh
@@ -1,5 +1,14 @@
-mkdir -p /kaniko/.dockerconfig && ln -s /kaniko/.docker/.dockerconfigjson /kaniko/.dockerconfig/config.json 
-IMAGE_ID=$1 && \ 
-IMAGE_TAG=$2 && \ 
-export DOCKER_CONFIG=/kaniko/.dockerconfig && \ 
-/kaniko/executor \ --context $(pwd) \ --dockerfile $(pwd)/Dockerfile \ --destination $IMAGE_ID:$IMAGE_TAG \ --force 
+
+mkdir -p /kaniko/.dockerconfig
+ln -s /kaniko/.docker/.dockerconfigjson /kaniko/.dockerconfig/config.json
+
+IMAGE_ID=$1
+IMAGE_TAG=$2
+
+export DOCKER_CONFIG=/kaniko/.dockerconfig
+
+/kaniko/executor \
+  --context="/home/jenkins/agent/workspace/kaniko/docker-images/jenkins-controller" \
+  --dockerfile="/home/jenkins/agent/workspace/kaniko/docker-images/jenkins-controller/Dockerfile" \
+  --destination=$IMAGE_ID:$IMAGE_TAG \
+  --force
